5aed5faab8767c05d96223f2057847d457d0c7d5
srijan116 <72309000+srijan116@users.noreply.github.com>
2024-03-03 16:08:11 +0530

    static files changed

--- static/js/script.js
+++ static/js/script.js
@@ -47,7 +47,7 @@ async function loadCards(){
         <div class="play">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="15" height="15"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
         </div>
-        <img src="/static/images/card-bg-${i+1}.jpg"" alt="artist" width="200" height="200">
+        <img src="/static/images/${cards[i]}.jpg"" alt="artist" width="200" height="200">
         <h4>${cards[i]}</h4>
         <p>Listen to songs by ${cards[i]}.</p>
     </div>`
